16019d1997779d460c724c3992d1be026aa7870f
maiphuocloi01 <maiphuocloi01@gmail.com>
2023-01-05 13:04:26 +0700

    update

--- app.py
+++ app.py
@@ -287,4 +287,5 @@ def predict():
 
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
+	#app.run(host='0.0.0.0', port=5000, debug=False, threaded=True)
+    app.run(debug=False)
